0c38c83dde5b10e3b0f296568a3d74d0fb1699fe
stancobridge <stancoproof@gmail.com>
2019-10-22 12:51:23 +0100

    Fixed comment

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
   res.send('<h1>Welcome to My Page, HaPpy HAcKiNg</h1>')
 });
 app.use('/user', apiRouter)
-// Favourite video router
+// Favourite video router for api
 app.use('/movie', favRoute)
 
 // Start server
